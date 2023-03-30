2421f241cd43dd099f4840717d9e9fd2aa9058d9
John-Giovenco <JohnGiovenco93@gmail.com>
2023-03-30 14:31:50 -0700

    laser works

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -1,19 +1,11 @@
 function newNonPlayableCharacter(x, y) {
-    let element = newImage('assets/red-character/static.gif')
+    let element = newImage('./asset/laser.png')
     element.style.zIndex = 1;
     
     let direction = null;
 
     function moveCharacter() {
-        if (direction === 'west') {
-            x -= 1
-        }
-        if (direction === 'north') {
-            y += 1
-        }
-        if (direction === 'east') {
-            x += 1
-        }
+        
         if (direction === 'south') {
             y -= 1
         }
@@ -23,46 +15,22 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    async function walkEast(time) {
-        direction = 'east'
-        element.src = `./assets/red-character/east.gif`
-        await sleep(time)
-        stop()
-    }
-
-    async function walkNorth(time) {
-        direction = 'north'
-        element.src = `./assets/red-character/north.gif`
-        await sleep(time)
-        stop()
-    }
-
-    async function walkWest(time) {
-        direction = 'west'
-        element.src = `./assets/red-character/west.gif`
-        await sleep(time)
-        stop()
-    }
-
     async function walkSouth(time) {
         direction = 'south'
-        element.src = `./assets/red-character/south.gif`
+        element.src = `./assets/laser.png`
         await sleep(time)
         stop()
     }
 
     async function stop() {
         direction = null
-        element.src = `./assets/red-character/static.gif`
+        element.src = `./assets/laser.png`
         await sleep(time)
         stop()
     }
 
     return {
         element: element,
-        walkWest: walkWest,
-        walkNorth: walkNorth,
-        walkEast: walkEast,
         walkSouth: walkSouth,
         stop: stop
     }
